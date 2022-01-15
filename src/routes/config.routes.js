a35718eeaa92cf17c6ee233320d58380fd8b898a
CarlosMontesdeoca <mateo.game@hotmail.com>
2022-01-14 21:05:38 -0500

    servidor app

--- src/routes/config.routes.js
+++ src/routes/config.routes.js
@@ -41,7 +41,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
 
     await Config.findByIdAndDelete(req.params.id);
-    res.json({ status: 'configuracion eliminada' })
+    res.json({ status: 'configuracion eliminada.' })
 
 })
 
