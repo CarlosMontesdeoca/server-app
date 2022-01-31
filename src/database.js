7e4342daac0dd7d6f42c47d37e216f7bd41757ba
CarlosMontesdeoca <mateo.game@hotmail.com>
2022-01-30 20:57:51 -0500

    cluster

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost/Apps';
+const URI = 'mongodb+srv://admin:admin123@cluster0.y3s4w.mongodb.net/test';
 
 mongoose.connect(URI)
   .then(db => console.log('Db is connected'))
